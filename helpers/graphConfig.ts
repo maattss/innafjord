42b32a65b6b109f3d1782722a9602315659b0e88
Mats Tyldum <mats.tyldum@bouvet.no>
2021-08-16 15:51:44 +0200

    Improve chart colors

--- helpers/graphConfig.ts
+++ helpers/graphConfig.ts
@@ -1,12 +1,23 @@
 export const graphOptions = {
   scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: false,
-        },
+    x: {
+      grid: {
+        color: "rgb(45, 55, 72)", // Horizontal grid colour
+        borderColor: "rgb(45, 55, 72)", // X-axis color
       },
-    ],
+      ticks: {
+        beginAtZero: false,
+      },
+    },
+    y: {
+      grid: {
+        color: "rgb(45, 55, 72)", // Horizontal grid colour
+        borderColor: "rgb(45, 55, 72)", // Y-axis color
+      },
+      ticks: {
+        beginAtZero: false,
+      },
+    },
   },
   plugins: {
     legend: {
@@ -21,8 +32,8 @@ export const getGraphDataset = (graphData: number[], graphLabels: string[]) => {
     datasets: [
       {
         data: graphData,
-        backgroundColor: "rgb(0, 181, 216, 0.5)",
-        borderColor: "rgb(0, 181, 216, 0.5)",
+        backgroundColor: "rgb(0, 181, 216, 0.5)", // Graph data points color
+        borderColor: "rgb(0, 181, 216, 0.5)", // Graph line color
       },
     ],
   };
